ab4db0f77ea5c7c99783a6ee0bdf68de95d16c12
Umadevi_veluduti <101104398+umadevi897@users.noreply.github.com>
2024-08-05 15:35:57 +0530

    Add files via upload
    taks 10 -part 1 and part 2
    task 11
    task 12

---
+++ list class 4 part 1 .py
@@ -0,0 +1,62 @@
+# List/class 4- list,list methods ,compherension/may 28 2024
+
+# list methods
+
+# append
+# uma=[1,2,3,4,5,6,7,8,9]
+# uma.append(123)
+# print(uma)
+
+# extend
+# uma=[1,2,3,4,5,6,7,8,9]
+# uma.extend([123])
+# print(uma) 
+
+# count
+# uma=[1,2,3,4,5,5,5,6,7,8,9]
+# print(uma.count(5))
+
+# length
+# uma=[1,2,3,4,5,6,7,8,9]
+# print(len(uma)) 
+
+# index
+# uma=[1,2,3,4,5,6,7,8,9]
+# print(uma.index(3))
+
+# pop
+# uma=[1,2,3,4,5,6,7,8,9]
+# uma.pop(4)
+# print(uma) 
+
+
+# remove
+# uma=[1,2,3,4,5,6,7,8,9]
+# uma.remove(5)
+# print(uma)
+
+# clear
+# uma=[1,2,3,4,5,6,7,8,9]
+# uma.clear()
+# print(uma)
+
+# reverse
+# uma=[1,2,3,4,5,6,7,8,9]
+# uma.reverse()
+# print(uma)
+
+# insert
+# uma=[1,2,3,4,5,6,7,8,9]
+# uma.insert(2,"devi")
+# print(uma)
+
+# sort ascending order
+# C=[1,2,3,4,5,6,7,8,9]
+# C.sort()
+# print(C)
+
+# sort desending order
+ #C=[1,2,3,4,5,6,7,8,9]
+ #C.sort(reverse=True)
+ #print(C)
+
---
+++ list compherension class 4 part 2.py
@@ -0,0 +1,31 @@
+# class 4
+# [x**2 for x in (1,2,3,4)]
+# print(list)
+
+# list /short hand if using and for loop
+# list = ["EVEN" if i % 2==0 else 'odd' for i in range(10)]
+# print(list)
+
+
+
+# fruits=["apple","banana","cherry","kiwi","mango"]
+# newlist=[x for x in fruits if "a" in x]
+# print(newlist)
+
+# index
+# s=[ 1,2,3,4,3,4]
+# for i in range(len(s)):
+#     if s[i]==3:
+#         print(i)
+
+# index/how many 6 are presented in string
+# str1=[1,6,6,6,6,2,4,5]
+# n=[]
+# for i in str1:
+#     if i==6:
+#        str1.remove(6)
+#     else:
+#         str1.remove(6)
+# else:
+#   n.append(i)
+# print(n)
